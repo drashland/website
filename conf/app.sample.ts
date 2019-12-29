e622b06bcae45070d32bc17e34a0aa4578eddf0c
Eric Crooks <eric.crooks@gmail.com>
2019-12-29 09:38:52 -0500

    improving dev server workflow

--- conf/app.sample.ts
+++ conf/app.sample.ts
@@ -3,8 +3,14 @@ export default {
     directory: "/var/www/deno-drash",
   },
   logger: {
-    enabled: false,
-    level: "info"
+    enabled: config.logger.enabled,
+    level: config.logger.level,
+    tag_string: "{date} | {level} |",
+    tag_string_fns: {
+      date: function() {
+        return new Date().toISOString().replace("T", " ");
+      }
+    }
   },
   server: {
     directory: "/var/www/deno-drash-docs"
