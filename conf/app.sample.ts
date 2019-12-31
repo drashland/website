e73ffde35b905c19e9d3a6123c213a8613a93d39
Eric Crooks <eric.crooks@gmail.com>
2019-12-31 00:31:10 -0500

    update sample

--- conf/app.sample.ts
+++ conf/app.sample.ts
@@ -1,18 +1,18 @@
 export default {
   deno_drash: {
-    directory: "/var/www/deno-drash",
+    directory: "/var/proj/deno-drash",
   },
   logger: {
-    enabled: config.logger.enabled,
-    level: config.logger.level,
+    enabled: false,
+    level: "info",
     tag_string: "{date} | {level} |",
     tag_string_fns: {
       date: function() {
         return new Date().toISOString().replace("T", " ");
-      }
-    }
+      },
+    },
   },
   server: {
-    directory: "/var/www/deno-drash-docs"
+    directory: "/var/proj/deno-drash-docs",
   }
 }
