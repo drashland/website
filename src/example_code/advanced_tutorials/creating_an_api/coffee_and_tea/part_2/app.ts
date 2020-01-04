d345daf1d0343f5733d9f10c9f67ae55a0785946
Eric Crooks <eric.crooks@gmail.com>
2020-01-04 00:36:58 -0500

    [ft-#5-creating-an-api-docs] uncomment

--- src/example_code/advanced_tutorials/creating_an_api/coffee_and_tea/part_2/app.ts
+++ src/example_code/advanced_tutorials/creating_an_api/coffee_and_tea/part_2/app.ts
@@ -1,14 +1,14 @@
 import Drash from "https://deno.land/x/drash/mod.ts";
 
-// import CoffeeResource from "./coffee_resource.ts";
-// import TeaResource from "./tea_resource.ts";
+import CoffeeResource from "./coffee_resource.ts";
+import TeaResource from "./tea_resource.ts";
 
 let server = new Drash.Http.Server({
   address: "localhost:1447",
   response_output: "application/json",
   resources: [
-    // CoffeeResource,
-    // TeaResource
+    CoffeeResource,
+    TeaResource
   ],
 });
 
