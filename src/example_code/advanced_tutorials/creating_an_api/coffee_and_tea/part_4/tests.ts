dc471ea38dd352c36b7dc626364234a6ccf159b3
Eric Crooks <eric.crooks@gmail.com>
2020-01-05 20:53:27 -0500

    [ft-#5-creating-an-api-docs] add tests

---
+++ src/example_code/advanced_tutorials/creating_an_api/coffee_and_tea/part_4/tests.ts
@@ -0,0 +1,27 @@
+let Drash = (await import(Deno.env().DENO_DRASH)).default;
+
+import response from "./response.ts";
+Drash.Http.Response = response;
+
+import CoffeeResource from "./coffee_resource.ts";
+import TeaResource from "./tea_resource.ts";
+
+let server = new Drash.Http.Server({
+  resources: [CoffeeResource, TeaResource]
+});
+
+let members = (await import(Deno.env().DENO_DRASH_DOCS_MEMBERS_PATH)).default;
+
+members.test("response", async () => {
+  let request = members.mockRequest("/coffee/17");
+  let actual = await server.handleHttpRequest(request);
+  // This test is failing because the tutorial does:
+  //
+  //     Deno.readFileSync("./coffee.json")
+  //
+  // The current working directory is being prepended to coffee.json like
+  //
+  //    /path/to/deno-drash-docs/coffee.json
+  //
+  members.assert.equals("test", members.decoder.decode(actual.body));
+});
---
+++ tests/run_tests.ts
@@ -0,0 +1,5 @@
+import members from "./members.ts";
+
+import "../src/example_code/advanced_tutorials/creating_an_api/coffee_and_tea/part_4/tests.ts";
+
+members.runTests();
