a1e9de22d7ec14da672991ea82fef23237e2319e
Eric Crooks <eric.crooks@gmail.com>
2020-01-20 22:33:37 -0500

    add dejs tests

--- tests/run_tests.ts
+++ tests/run_tests.ts
@@ -2,6 +2,7 @@ import members from "./members.ts";
 
 import "../src/example_code/third_party_tutorials/databases/deno_mysql_test/app.ts";
 import "../src/example_code/third_party_tutorials/databases/deno_postgres_test/app.ts";
+import "../src/example_code/third_party_tutorials/template_engines/dejs_test/app.ts";
 
 members.runTests();
 
