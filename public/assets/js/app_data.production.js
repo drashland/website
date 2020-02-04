51a2b0cee8406270ca9fa5272a76304d92f5137a
Eric Crooks <eric.crooks@gmail.com>
2020-02-03 22:34:08 -0500

    Mon Feb  3 22:34:08 EST 2020 - manual build

--- public/assets/js/app_data.production.js
+++ public/assets/js/app_data.production.js
@@ -2663,8 +2663,8 @@ const app_data = {
             "description": [
               "`enabled`: Is the logger enabled? This is useful if you have a config\nfile that can toggle this option between `true` and `false`.",
               "`level`: Options are `all`, `trace`, `debug`, `info`, `warn`, `error`, `fatal`, and `off`.",
-              "`tag_string`: This only takes a string with tags as `{tag}`. For example, `{some_tag} | {some_tag} * {some_tag} [{some_tag}`.",
-              "`tag_string_fns`: This takes an object of key-value pairs whwere the key"
+              "`tag_string`: This only takes a string with tags as `{tag}`. For example, `{some_tag} | {some_tag} * {some_tag} [{some_tag}]`.",
+              "`tag_string_fns`: This takes an object of key-value pairs where the key is the name of the tag defined in the `tag_string` config. This object is used to replace tags in the `tag_string` config by matching keys to tags and replacing tags with the values of the keys. For example, if `tag_string` was `{my_cool_tag}` and `tags_string_fns.my_cool_tag` returns `\"HELLO\"`, then `{my_cool_tag}` would be replaced with `HELLO`."
             ],
             "signature": "export interface LoggerConfigs {\n  enabled: boolean;\n  level: string;\n  tag_string: string;\n  tag_string_fns: any;\n}",
             "is_interface": true,
