9e95c890c416ce1c39eacac009b6884f761ac7a6
eric.crooks@gmail.com <--replace-all>
2020-03-29 22:42:12 -0400

    Sun Mar 29 22:42:12 EDT 2020 - manual build

--- docs/conf/app.sample.ts
+++ docs/conf/app.sample.ts
@@ -1,7 +1,7 @@
 export default {
   example_code_versions: {
-    deno: "v0.37.1",
-    drash: `import Drash from "https://deno.land/x/drash@v0.37.1/mod.ts";`
+    deno: "v0.38.0",
+    drash: `import Drash from "https://deno.land/x/drash@v0.38.0/mod.ts";`
   },
   deno_drash: {
     directory: "/var/src/drashland/deno-drash",
