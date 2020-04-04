fb88f8d4996267e85708a5a95467be0b572d2867
eric.crooks@gmail.com <--replace-all>
2020-04-04 10:08:53 -0400

    fix method name

--- docs/src/example_code/tutorials/middleware/adding_resource_level_middleware/verify_token_middleware.ts
+++ docs/src/example_code/tutorials/middleware/adding_resource_level_middleware/verify_token_middleware.ts
@@ -2,7 +2,7 @@ import Drash from "https://deno.land/x/drash/mod.ts";
 
 export default class VerifyTokenMiddleware extends Drash.Http.Middleware {
   public run() {
-    let token = this.request.getQueryParam('super_secret_token');
+    let token = this.request.getUrlQueryParam('super_secret_token');
 
     if (!token) {
       throw new Drash.Exceptions.HttpMiddlewareException(400, "Where is the token?");
