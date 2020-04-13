6c0983a2516932f2cf40edd07ec1f12a4eed1f14
Edward Bebbington <edward.bebbington@intercity.technology>
2020-04-14 00:14:15 +0100

    Added src directory for example code for docker tutorial with app file

---
+++ docs/src/example_code/advanced_tutorials/creating_a_docker_container/docker_compose_setup/part_6/src/app.ts
@@ -0,0 +1,23 @@
+import Drash from "https://deno.land/x/drash/mod.ts";
+
+class HomeResource extends Drash.Http.Resource {
+
+  static paths = [
+    "/"
+  ];
+
+  public GET() {
+    this.response.body = 'Hello World!';
+    return this.response;
+  }
+}
+
+const server = new Drash.Http.Server({
+  address: ":1447",
+  response_output: "text/html",
+  resources: [
+    HomeResource
+  ],
+});
+
+server.run();
