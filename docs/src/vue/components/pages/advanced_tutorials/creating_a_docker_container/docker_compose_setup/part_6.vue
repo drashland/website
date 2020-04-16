5675c0efec8a4e791ee0a587cb89d4577d19b723
eric.crooks@gmail.com <--replace-all>
2020-04-15 21:04:31 -0400

    edits

--- docs/src/vue/components/pages/advanced_tutorials/creating_a_docker_container/docker_compose_setup/part_6.vue
+++ docs/src/vue/components/pages/advanced_tutorials/creating_a_docker_container/docker_compose_setup/part_6.vue
@@ -55,54 +55,54 @@ page-tutorial-part(
       h2-hash Steps
       ol
         li
-          p Modify the content response type in your Drash server creation and update the <code>GET</code> request method
+          p Modify the content response type in your Drash server configs and update the <code>GET</code> request method.
           p
             code-block-slotted(language="typescript" line_highlight="10-14,20")
               template(v-slot:title) /path/to/your/project/src/app.ts
               template(v-slot:code)
                 | {{ example_code_src.app.contents }}
         li
-          p Added the <code>--allow-read</code> flag in your <code>docker-compose.yml</code> file
+          p Add the <code>--allow-read</code> flag in your <code>docker-compose.yml</code> file.
           p
             code-block-slotted(language="typescript" line_highlight="9")
               template(v-slot:title) /path/to/your/project/docker-compose.yml
               template(v-slot:code)
                 | {{ example_code.docker_compose.contents }}
         li
-          p Create your HTML file
+          p Create your HTML file.
           p
             code-block-slotted(language="html")
               template(v-slot:title) /path/to/your/project/src/index.html
               template(v-slot:code)
                 | {{ example_code_html.index.contents }}
         li
-          p Create your CSS file
+          p Create your CSS file.
           p
             code-block-slotted(language="css")
               template(v-slot:title) /path/to/your/project/src/index.css
               template(v-slot:code)
                 | {{ example_code_css.index.contents }}
         li
-          p Create your JS file
+          p Create your JS file.
           p
             code-block-slotted(language="javascript")
               template(v-slot:title) /path/to/your/project/src/index.js
               template(v-slot:code)
                 | {{ example_code_js.index.contents }}
         li
-          p Update your Nginx configuration
-          p You only need to do this if your are using Nginx as your reverse proxy server.
-          p On any request for a <code>.js</code> or <code>.css</code>, for example <code>index.js</code>, will be passed through this location block which will make Nginx look for the requested file in that directory specified.
+          p Update your Nginx configuration.
+          p You only need to do this if you are using Nginx as your reverse proxy server.
+          p Any request for a <code>.js</code> or <code>.css</code> file (e.g., <code>index.js</code>) will pass through this location block. This will make Nginx look for the requested file in the specified directory.
           p
             code-block-slotted(language="shell" line_highlight="3-5")
               template(v-slot:title) /path/to/your/project/.docker/conf/nginx.conf
               template(v-slot:code)
                 | {{ example_code.nginx.contents }}
         li
-          p Update your Apache configuration
+          p Update your Apache configuration.
           p You only need to do this if you are using Apache as your reverse proxy server.
-          p These new configuration lines will make sure any requests that are for a <code>.js</code> or a <code>.css</code> will not be passed to your Drash server and will instead be handled by your Apache server.
-          p You should note that the below does allow Apache to access the files as it is required for when a request for the JavaScripts or StyleSheets come in, but is very insecure as it is allowing full access to your whole source code. Ideally you would place your assets in a <code>public/</code> folder and modify the below to match this. That way you would only allow your public files to be accessible and you would link your files in your HTML like such: <code>/public/index.css</code>.
+          p These new configuration lines will make sure any requests for a <code>.js</code> or a <code>.css</code> file will not be passed to your Drash server and will instead be handled by your Apache server.
+          p You should note that the below does allow Apache to access the files as it is required for when a request for the JavaScripts or StyleSheets come in, but is very insecure as it is allowing full access to your source code. Ideally, you would place your assets in a <code>/public</code> directory and modify the below to match this. That way you would only allow your public files to be accessible and you would link your files in your HTML like so: <code>/public/index.css</code>.
           p
             code-block-slotted(language="shell")
               template(v-slot:title) /path/to/your/project/.docker/conf/apache.conf
@@ -121,7 +121,7 @@ page-tutorial-part(
               template(v-slot:code)
                 | docker-compose build
         li
-          p Start docker
+          p Start docker.
           p
             code-block-slotted
               template(v-slot:title) Terminal
