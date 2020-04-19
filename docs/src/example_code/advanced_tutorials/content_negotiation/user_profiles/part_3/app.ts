c995119e0c0d09c7ee7a9de93b630a0bf1548a3f
eric.crooks@gmail.com <--replace-all>
2020-04-18 22:01:13 -0400

    remove address

--- docs/src/example_code/advanced_tutorials/content_negotiation/user_profiles/part_3/app.ts
+++ docs/src/example_code/advanced_tutorials/content_negotiation/user_profiles/part_3/app.ts
@@ -3,7 +3,6 @@ import { Drash } from "https://deno.land/x/drash/mod.ts";
 import UsersResource from "./users_resource.ts";
 
 const server = new Drash.Http.Server({
-  address: "localhost:1447",
   response_output: "application/json",
   resources: [UsersResource],
 });
