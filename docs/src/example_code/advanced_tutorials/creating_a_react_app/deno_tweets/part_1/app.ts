3b9e96755cc0ce8f9eab54f12cece9438ee3f0fd
Edward Bebbington <edward.bebbington@intercity.technology>
2020-04-19 20:27:35 +0100

    Created part 1 example code for react tutorial

---
+++ docs/src/example_code/advanced_tutorials/creating_a_react_app/deno_tweets/part_1/app.ts
@@ -0,0 +1,15 @@
+import { Drash } from "https://deno.land/x/drash/mod.ts";
+
+import HomeResource from "./home_resource.ts";
+
+const server = new Drash.Http.Server({
+  response_output: "text/html",
+  resources: [HomeResource],
+  directory: ".",
+  static_paths: ["/public"]
+});
+
+server.run({
+  hostname: "localhost",
+  port: 1447
+});
