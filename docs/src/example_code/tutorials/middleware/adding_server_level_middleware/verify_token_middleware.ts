09b6c9d3c5b4951b8141e77b08660c7955604047
eric.crooks@gmail.com <--replace-all>
2020-04-23 18:52:19 -0400

    initial commit

--- docs/src/example_code/tutorials/middleware/adding_server_level_middleware/verify_token_middleware.ts
+++ docs/src/example_code/tutorials/middleware/adding_server_level_middleware/verify_token_middleware.ts
@@ -1,15 +1,16 @@
 import { Drash } from "https://deno.land/x/drash/mod.ts";
 
-export default class VerifyTokenMiddleware extends Drash.Http.Middleware {
-  public run() {
-    let token = this.request.getUrlQueryParam('super_secret_token');
+export function VerifyTokenMiddleware(
+  request: any,
+  response?: Drash.Http.Response
+): void {
+  let token = request.getUrlQueryParam('super_secret_token');
 
-    if (!token) {
-      throw new Drash.Exceptions.HttpMiddlewareException(400, "Where is the token?");
-    }
+  if (!token) {
+    throw new Drash.Exceptions.HttpMiddlewareException(400, "Where is the token?");
+  }
 
-    if (token != "AllYourBaseAreBelongToUs") {
-      throw new Drash.Exceptions.HttpMiddlewareException(403, `Mmm... "${token}" is a bad token.`);
-    }
+  if (token != "AllYourBaseAreBelongToUs") {
+    throw new Drash.Exceptions.HttpMiddlewareException(403, `Mmm... "${token}" is a bad token.`);
   }
 }
