ad2db0ad32f0cb3a487f05f25e5345cd351a74c9
Eric Crooks <eric.crooks@gmail.com>
2020-06-19 08:25:48 -0400

    fix description

--- docs/src/vue/components/pages/about_drash.vue
+++ docs/src/vue/components/pages/about_drash.vue
@@ -31,7 +31,7 @@ page(
     div.col
       hr
       h2-hash What is Drash?
-      p Drash is a REST microframework for <a href="https://deno.land" target="_BLANK">Deno</a> with zero dependencies.
+      p Drash is a REST microframework for <a href="https://deno.land" target="_BLANK">Deno</a>'s HTTP server with zero dependencies.
       p Drash is designed to help you build your projects quickly with the ability to scale. You can build an API, a web app, an SPA (like these documentation pages), or even a static HTML site. How you use Drash is up to you, so it can be everything you need and nothing you don't &mdash; like a DRASH tent.
   div.row
     div.col
