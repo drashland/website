a4dfc9482e8131aa6903fdcf2203435872829244
Eric Crooks <eric.crooks@gmail.com>
2020-07-04 06:38:07 -0400

    change docs to drash

---
+++ drash/conf/env_vars_version.ts
@@ -0,0 +1,8 @@
+const env = {
+  "base_url": "/drash/versions/v1.x",
+  "environment": "production",
+  "module_name": "Drash",
+  "requirements_url": "https://github.com/drashland/deno-drash/blob/master/REQUIREMENTS.md"
+};
+
+export default env;
