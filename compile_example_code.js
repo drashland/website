bb363b4f01699e30512eedcacfde1ca8b51ae417
Eric Crooks <eric.crooks@gmail.com>
2020-07-04 19:09:49 -0400

    use configs to determine latest drash URL

--- compile_example_code.js
+++ compile_example_code.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const directory = "drash/example_code";
 const outFile = "drash/assets/js/compiled_example_code.js";
+const configs = require("./configs.json");
 
 console.log(`Compiling ${outFile}...`);
 
@@ -22,8 +23,10 @@ function walk(directory) {
     if (stats.isDirectory()) {
       walk(filepath);
     } else if (stats.isFile()) {
+      let contents = fs.readFileSync(filepath, "utf8");
+      contents = contents.replace("https://deno.land/x/drash/mod.ts", configs.drash.latest_url_deno_land);
       exampleCode[fileNamespace][filenameWithoutExtension] = {
-        contents: fs.readFileSync(filepath, "utf8"),
+        contents: contents,
       };
     }
   }
