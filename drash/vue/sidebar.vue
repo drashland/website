d26ed2dbb8145e8cc7596fea391cb0e5c2c9b981
Eric Crooks <eric.crooks@gmail.com>
2020-07-05 07:00:22 -0400

    remove issues from docs

--- drash/vue/sidebar.vue
+++ drash/vue/sidebar.vue
@@ -23,7 +23,6 @@ div.c-sidebar
         a-base-url(href="/about-drash") About Drash
         a-base-url(href="/lifecycle-diagram") Drash Lifecycle Diagram
         a-base-url(href="/dependency-management") Dependency Management
-        a-base-url(href="/issues") Issues
         a(href="https://github.com/drashland/deno-drash" target="_BLANK") View On GitHub &mdash; Drash
         a(href="https://github.com/drashland/deno-drash-docs" target="_BLANK") View On GitHub &mdash; Drash Docs
       span.heading
