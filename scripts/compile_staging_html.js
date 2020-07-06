7134bb497ca25d8875fbe4937fefd20062155e13
Eric Crooks <eric.crooks@gmail.com>
2020-07-05 20:05:34 -0400

    build all staging envs

--- scripts/compile_staging_html.js
+++ scripts/compile_staging_html.js
@@ -1,25 +1,26 @@
 const fs = require("fs");
+let html;
 
 // Create dmm's staging.html file
-let html = fs.readFileSync("./dmm/index.template.html", "utf8");
+html = fs.readFileSync("./dmm/index.template.html", "utf8");
 html = html.replace(/\{\{ environment \}\}/g, "staging");
 html = html.replace(/\{\{ version \}\}/g, new Date().getTime());
 fs.writeFileSync("./dmm/staging.html", html);
 
 // Create Drash's staging.html file
-let html = fs.readFileSync("./drash/index.template.html", "utf8");
+html = fs.readFileSync("./drash/index.template.html", "utf8");
 html = html.replace(/\{\{ environment \}\}/g, "staging");
 html = html.replace(/\{\{ version \}\}/g, new Date().getTime());
 fs.writeFileSync("./drash/staging.html", html);
 
 // Create Rhum's staging.html file
-let html = fs.readFileSync("./rhum/index.template.html", "utf8");
+html = fs.readFileSync("./rhum/index.template.html", "utf8");
 html = html.replace(/\{\{ environment \}\}/g, "staging");
 html = html.replace(/\{\{ version \}\}/g, new Date().getTime());
 fs.writeFileSync("./rhum/staging.html", html);
 
 // Create Sockets's staging.html file
-let html = fs.readFileSync("./sockets/index.template.html", "utf8");
+html = fs.readFileSync("./sockets/index.template.html", "utf8");
 html = html.replace(/\{\{ environment \}\}/g, "staging");
 html = html.replace(/\{\{ version \}\}/g, new Date().getTime());
 fs.writeFileSync("./sockets/staging.html", html);
