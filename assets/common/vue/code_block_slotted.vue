66992a1ae7e9c1d006b43083ed52cf6186f01ebe
Eric Crooks <eric.crooks@gmail.com>
2020-07-13 06:19:01 -0400

    start building the common components and assets

---
+++ assets/common/vue/code_block_slotted.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  name: "CodeBlockSlotted",
+  props: [
+    'header',
+    'line_highlight',
+    'language',
+  ],
+  mounted() {
+    Prism.highlightAll();
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+    pre {
+        margin-bottom: 0;
+    }
+    .no-header {
+        border-radius: 1.0875rem;
+    }
+</style>
+
+<template lang="pug">
+div.b-code-example.code-block-default
+    pre.header(v-if="header !== false")
+        code.header
+            slot(name="title")
+    pre.body(:data-line="line_highlight" :class="{'no-header': header === false}")
+        code(:class="'language-' + language")
+            slot(name="code")
+</template>
