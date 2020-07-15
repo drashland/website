1b8949869a6ccbf60d82c5d5601a7bd8048de133
Edward Bebbington <edward.bebbington@intercity.technology>
2020-07-15 10:25:33 +0100

    [add-dmm-docs] Added app.vue

---
+++ dmm/vue/app.vue
@@ -0,0 +1,45 @@
+<script>
+import AppRoot from "/common/vue/app_root.vue";
+
+export default {
+  components: {
+    AppRoot,
+  },
+  data() {
+    return {
+      build_date: this.$conf.build_date,
+      environment: this.$conf.environment,
+      sidebar: {
+        base_url: this.$conf.dmm.base_url + "/#",
+        github_href: "https://github.com/drashland/dmm",
+        logo: "/assets/common/img/logo_dmm.svg",
+        menus: {
+          "Getting Started": {
+            Quickstart: "/#quickstart",
+            Installing: "/#installing",
+            Features: "/#features",
+            FAQ: "/faq",
+          },
+          "CLI Commands": {
+            //Introduction: "/tutorials",
+            "info": "/tutorials/info",
+            "check": "/tutorials/check",
+            "update": "/tutorials/update",
+            "--help": "/tutorials/help",
+            "--version": "/tutorials/version"
+          },
+        },
+        module: "dmm",
+      }
+    };
+  },
+}
+</script>
+
+<template lang="pug">
+app-root(
+  :build_date="build_date"
+  :environment="environment"
+  :sidebar="sidebar"
+)
+</template>
