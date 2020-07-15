03141cb1eadaebf5048c6df6b6dbb44289cd9702
Edward Bebbington <edward.bebbington@intercity.technology>
2020-07-15 13:48:21 +0100

    [add-dmm-docs] Fix api reference item in sidebar

--- assets/common/vue/sidebar.vue
+++ assets/common/vue/sidebar.vue
@@ -120,9 +120,8 @@ div.sidebar(:style="'background-color: ' + styles.background_color + ';'")
       ul.mb-0
         li.menu-item(v-for="(href, link_text) in sub_menu_items")
           a.menu-item-link(:href="base_url + href" @click="closeSidebar()") {{ link_text }}
-    if api_reference_href
-      div.menu-name
-        a.menu-name-link.is-link(:href="api_reference_href" @click="closeSidebar()") API Reference
+    div.menu-name(v-if="api_reference_href")
+      a.menu-name-link.is-link(:href="api_reference_href" @click="closeSidebar()") API Reference
     div.menu-name
       a.menu-name-link.is-link(:href="github_href" @click="closeSidebar()") GitHub
     div.menu-name
