34928696255528200b92a0dd0fe656ca186041bc
Edward Bebbington <edward.bebbington@intercity.technology>
2020-07-15 10:42:49 +0100

    [add-dmm-docs] Add page for version command

---
+++ dmm/vue/pages/cli_commands/version.vue
@@ -0,0 +1,53 @@
+<script>
+  import H2Hash from "/common/vue/h2_hash.vue";
+  import Page from "/common/vue/page.vue";
+  import CodeBlock from "/common/vue/code_block.vue";
+
+  const title = "--version";
+
+  export const resource = {
+    paths: [
+      "/cli_commands/version",
+    ],
+    meta: {
+      title: title
+    }
+  }
+
+  export default {
+    components: {
+      CodeBlock,
+      H2Hash,
+      Page,
+    },
+    data() {
+      return {
+        base_url: this.$conf.dmm.base_url + "/#",
+        title: title,
+        toc: [
+          "Description",
+          "Example",
+        ],
+      };
+    }
+  }
+</script>
+
+<template lang="pug">
+page(
+  :base_url="base_url"
+  :title="title"
+  :toc="toc"
+)
+  h2-hash Description
+  p The <code>--version</code> command will show the current version of dmm
+  hr
+  h2-hash Example
+  ul
+    li
+      p Check your version.
+      code-block(title="terminal" language="terminal")
+        | $ dmm --version
+        | dmm {version}
+</template>
+
