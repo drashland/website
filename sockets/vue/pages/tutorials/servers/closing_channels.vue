0920cc25949ee5475c629791df169e5fe8464526
Eric Crooks <eric.crooks@gmail.com>
2020-07-19 22:59:03 -0400

    add closing channels tutorial

---
+++ sockets/vue/pages/tutorials/servers/closing_channels.vue
@@ -0,0 +1,46 @@
+<script>
+import H2Hash from "/common/vue/h2_hash.vue";
+import Page from "/common/vue/page.vue";
+import CodeBlock from "/common/vue/code_block.vue";
+
+const title = "Closing Channels";
+
+export const resource = {
+  paths: [
+    "/tutorials/servers/closing-channels",
+  ],
+  meta: {
+    title: title
+  }
+}
+
+export default {
+  components: {
+    CodeBlock,
+    H2Hash,
+    Page,
+  },
+  data() {
+    return {
+      base_url: this.$conf.sockets.base_url + "/#",
+      title: title,
+      toc: [
+        "Closing Channels",
+      ],
+    };
+  }
+}
+</script>
+
+<template lang="pug">
+page(
+  :base_url="base_url"
+  :title="title"
+  :toc="toc"
+)
+  h2-hash Closing Channels
+  p Closing channels can be done using the following call:
+  code-block(:header="false" language="typescript")
+    | socketServer.closeChannel("Channel Name");
+</template>
+
