e2c694491ee03f3e0c6114a0456dd06ebd6dea62
Eric Crooks <eric.crooks@gmail.com>
2020-07-19 21:15:19 -0400

    make quickstart example quicker

--- sockets/vue/pages/getting_started.vue
+++ sockets/vue/pages/getting_started.vue
@@ -20,35 +20,6 @@ export default {
   },
   data() {
     return {
-      set_up_front_end: `<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8"/>
-  <meta name="viewport" content="width=device-width, minimum-scale=1.0, user-scalable=no"/>
-  <title>Sockets</title>
-</head>
-<body>
-  <div id="messages"></div>
-  <script>
-    let socketClient = null;
-  <\/script>
-  <script type="module">
-    import SocketClient from "https://cdn.jsdelivr.net/gh/drashland/sockets-client@latest/client.js";
-
-    socketClient = new SocketClient({ hostname: 'localhost', port: 1777 });
-
-    socketClient.on("Channel 1", (incomingMessage) => {
-      console.log(
-        "Message received from the server: " + JSON.stringify(incomingMessage),
-      );
-      const messages = document.getElementById("messages");
-      const li = document.createElement("li");
-      li.appendChild(document.createTextNode(incomingMessage.text));
-      messages.appendChild(li);
-    });
-  <\/script>
-</body>
-</html>`
     };
   }
 }
@@ -86,44 +57,27 @@ div
           |   hostname: "127.0.0.1",
           |   port: 1777,
           | });
+          |
           | console.log(
           |   `Socket server started on ws://${socketServer.hostname}:${socketServer.port}`,
           | );
-          | 
-          | // Create Channel 1 so that clients can send messages to it
-          | socketServer.createChannel("Channel 1");
-          | 
-          | // Add a handler for messages sent to Channel 1
-          | socketServer.on("Channel 1", (packet: IPacket) => {
-          |   console.log(packet);
-          |   // Do something with the packet (in this case, we're just confirming receipt and sending the message back)
-          |   socketServer.to(
-          |     packet.to, // => Channel 1
-          |     `Message received from client #${packet.from}: ${packet.message}`,
-          |   );
-          | });
-        li
-          p Run your socket server.
-          code-block(title="Terminal" language="text")
-            | deno run --allow-net app.ts
-        li
-          p Install <code>wscat</code> or similar client to interact with your socket server.
-          code-block(title="Terminal" language="text")
-            | npm install -g wscat
-        li
-          p Connect to your socket server.
-          code-block(title="Terminal" language="text")
-            | wscat -c ws://127.0.0.1:1777
-        li
-          p Connect to Channel 1.
-          code-block(title="Terminal" language="text")
-            | > {"connect_to":["Channel 1"]}
-            | < Connected to Channel 1.
-        li
-          p Send a message to Channel 1.
-          code-block(title="Terminal" language="text")
-            | > {"send_message":{"to":["Channel 1"],"message":"Hello World!"}}
-            | < {"from":"Server","to":"Channel 1","message":"Message received from client #4: Hello World!"}
+      li
+        p Run your socket server.
+        code-block(title="Terminal" language="text")
+          | deno run --allow-net app.ts
+      li
+        p Install <code>wscat</code> or similar client to interact with your socket server.
+        code-block(title="Terminal" language="text")
+          | npm install -g wscat
+      li
+        p Connect to your socket server.
+        code-block(title="Terminal" language="text")
+          | wscat -c ws://127.0.0.1:1777
+      li
+        p Ping your socket server.
+        code-block(title="Terminal" language="text")
+          | > ping
+          | < pong
     hr
     h2-hash Importing
     code-block-import(name="Sockets" repo="sockets")
