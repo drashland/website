cc1c561fcaf2d16624d7695b2a0c90883905371b
Eric Crooks <eric.crooks@gmail.com>
2020-07-19 23:44:57 -0400

    add adding/removing clients to/from channels

---
+++ sockets/vue/pages/tutorials/servers/adding_clients_to_channels.vue
@@ -0,0 +1,46 @@
+<script>
+import H2Hash from "/common/vue/h2_hash.vue";
+import Page from "/common/vue/page.vue";
+import CodeBlock from "/common/vue/code_block.vue";
+
+const title = "Adding Clients To Channels";
+
+export const resource = {
+  paths: [
+    "/tutorials/servers/adding-clients-to-channels",
+  ],
+  meta: {
+    title: title
+  }
+}
+
+export default {
+  components: {
+    CodeBlock,
+    H2Hash,
+    Page,
+  },
+  data() {
+    return {
+      base_url: this.$conf.sockets.base_url + "/#",
+      title: title,
+      toc: [
+        "Adding Clients To Channels",
+      ],
+    };
+  }
+}
+</script>
+
+<template lang="pug">
+page(
+  :base_url="base_url"
+  :title="title"
+  :toc="toc"
+)
+  h2-hash Adding Clients To Channels
+  p Adding clients to channels can be done using the following call:
+  code-block(:header="false" language="typescript")
+    | socketServer.addClientToChannel("Channel Name", clientId);
+</template>
+
---
+++ sockets/vue/pages/tutorials/servers/removing_clients_from_channels.vue
@@ -0,0 +1,46 @@
+<script>
+import H2Hash from "/common/vue/h2_hash.vue";
+import Page from "/common/vue/page.vue";
+import CodeBlock from "/common/vue/code_block.vue";
+
+const title = "Removing Clients From Channels";
+
+export const resource = {
+  paths: [
+    "/tutorials/servers/removing-clients-from-channels",
+  ],
+  meta: {
+    title: title
+  }
+}
+
+export default {
+  components: {
+    CodeBlock,
+    H2Hash,
+    Page,
+  },
+  data() {
+    return {
+      base_url: this.$conf.sockets.base_url + "/#",
+      title: title,
+      toc: [
+        "Removing Clients From Channels",
+      ],
+    };
+  }
+}
+</script>
+
+<template lang="pug">
+page(
+  :base_url="base_url"
+  :title="title"
+  :toc="toc"
+)
+  h2-hash Removing Clients To Channels
+  p Removing clients to channels can be done using the following call:
+  code-block(:header="false" language="typescript")
+    | socketServer.removeClientFromChannel("Channel Name", clientId);
+</template>
+
