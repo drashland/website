8ddc89b1b687ddbf58142a45436e002914a85685
Eric Crooks <eric.crooks@gmail.com>
2020-10-11 08:41:54 -0400

    finish creating a react app tutorial

--- drash/vue/pages/advanced_tutorials/creating_a_react_app/deno_tweets/part_5.vue
+++ drash/vue/pages/advanced_tutorials/creating_a_react_app/deno_tweets/part_5.vue
@@ -1,94 +1,94 @@
 <script>
+const title = "Creating A Client Side Rendered App (React)";
+const subtitle = "Part 5: Creating The React Components";
+const baseUri = "/advanced-tutorials/creating-a-client-side-rendered-app-react";
+
 export const resource = {
-    paths: ["/advanced-tutorials/creating-a-react-app/deno-tweets/part-5"],
+    paths: ["/advanced-tutorials/creating-a-client-side-rendered-app-react/part-5"],
     meta: {
-      title: "Creating A React App",
-      subtitle: "Part 5: Creating the React Components",
-      source_code_uri: "/advanced_tutorials/creating_a_react_app/deno_tweets/part_5"
+      title,
+      subtitle,
     }
 }
 
 export default {
   data() {
     return {
-      example_code: this.$example_code['drash/example_code' + resource.meta.source_code_uri],
-      example_code_components: this.$example_code['drash/example_code' + resource.meta.source_code_uri + '/components'],
-      part: 5,
-      parts: 5,
-      toc: {
-        items: [
-          "Before You Get Started",
-          "Folder Structure End State",
-          "Steps",
-          "Verification",
-        ]
-      },
-      uri: this.$conf.drash.base_url + "/#/advanced-tutorials/creating-a-react-app/deno-tweets"
+      base_url: this.$conf.drash.base_url + "/#",
+      base_uri: baseUri,
+      example_code: this.$example_code['drash/example_code/advanced_tutorials/creating_a_react_app/deno_tweets/part_5'],
+      example_code_components: this.$example_code['drash/example_code/advanced_tutorials/creating_a_react_app/deno_tweets/part_5/components'],
+      toc: [
+        "Before You Get Started",
+        "Folder Structure End State",
+        "Steps",
+        "Verification",
+      ],
+      title,
+      subtitle,
     };
   },
 }
 </script>
 
 <template lang="pug">
-page-tutorial-part(
-  :part="part"
-  :parts="parts"
+page(
+  :base_url="base_url",
+  :base_uri="base_uri",
+  :subtitle="subtitle"
+  :title="title"
   :toc="toc"
-  :uri="uri"
 )
-  div.row
-    div.col
-      hr
-      h2-hash Before You Get Started
-      p Now that you have everything set up, you can start writing your React components in TSX.
-      p-view-source-code
-  div.row
-    div.col
-      hr
-      div-folder-structure-end-state(:code_block_data="example_code.folder_structure")
-  div.row
-    div.col
-      hr
-      h2-hash Steps
-      ol
-        li
-          p Create your React component for the Deno Tweets.
-          p
-            code-block-slotted(language="typescript")
-              template(v-slot:title) /path/to/your/project/components/DenoTweets.tsx
-              template(v-slot:code)
-                | {{ example_code_components.DenoTweets.contents }}
-          p Here, you are creating a component to control the display of the Deno tweets.
-        li
-          p Create your React App file to handle the display of your components.
-          p
-            code-block-slotted(language="typescript")
-              template(v-slot:title) /path/to/your/project/components/App.tsx
-              template(v-slot:code)
-                | {{ example_code_components.App.contents }}
-          p This is the entry point React file. This file will import all of your React components and render them.
-  div.row
-    div.col
-      hr
-      h2-hash Verification
-      ol
-        li
-          p Build your environment.
-          p
-            code-block-slotted
-              template(v-slot:title) Terminal
-              template(v-slot:code)
-                | node_modules/.bin/webpack-cli --config webpack.config.js
-          p Now that you have created your components, Webpack will transpile them so the browser can understand the contents.
-        li
-          p Start your server.
-          p
-            code-block-slotted
-              template(v-slot:title) Terminal
-              template(v-slot:code)
-                | deno run --allow-net --allow-read app.ts
-          p Go to <code>localhost:1447/</code> in your browser.
-          p You should the something similar to the following:
-          p
-            img(:src="'/drash/assets/img/example_code/advanced_tutorials/creating_a_react_app/deno_tweets/1.png'")
+  breadcrumbs(:base_url="base_url + base_uri" :part="5" :parts="5")
+  hr
+  h2-hash Before You Get Started
+  p Now that you have everything set up, you can start writing your React components in TSX.
+  hr
+  h2-hash Folder Structure End State
+  code-block(:header="false" language="text" :line_numbers="false")
+    | ▾ /path/to/your/project/
+    |     app.ts
+    |     ▾ components/
+    |         App.tsx
+    |         DenoTweets.tsx
+    |     home_resource.ts
+    |     index.html
+    |     tsconfig.json
+    |     webpack.config.js
+  hr
+  h2-hash Steps
+  ol
+    li
+      p Create your React component for the Deno Tweets.
+      p
+        code-block(title="/path/to/your/project/components/DenoTweets.tsx" language="typescript")
+          | {{ example_code_components.DenoTweets.contents }}
+      p Here, you are creating a component to control the display of the Deno tweets.
+    li
+      p Create your React App file to handle the display of your components.
+      p
+        code-block(title="/path/to/your/project/components/App.tsx" language="typescript")
+          | {{ example_code_components.App.contents }}
+      p This is the entry point React file. This file will import all of your React components and render them.
+  hr
+  h2-hash Verification
+  ol
+    li
+      p Build your environment.
+      p
+        code-block(title="Terminal")
+          | node_modules/.bin/webpack-cli --config webpack.config.js
+      p Now that you have created your components, Webpack will transpile them so the browser can understand the contents.
+    li
+      p Start your server.
+      p
+        code-block(title="Terminal")
+          | deno run --allow-net --allow-read app.ts
+      p Go to <code>localhost:1447/</code> in your browser.
+      p You should the something similar to the following:
+      p
+        img(:src="'/drash/assets/img/example_code/advanced_tutorials/creating_a_react_app/deno_tweets/1.png'")
+  div-alert-finished-tutorial-series
+  hr
+  breadcrumbs(:base_url="base_url + base_uri" :part="5" :parts="5")
 </template>
