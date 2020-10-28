2b9926074c1017ff762ec2bb3fb2361b5654f432
Edward Bebbington <edward.bebbington@intercity.technology>
2020-10-28 22:03:54 +0000

    [issue-#197-improve-breadcrumbs] Fix selcted part having no margin-right

--- assets/common/vue/breadcrumbs.vue
+++ assets/common/vue/breadcrumbs.vue
@@ -23,13 +23,13 @@ export default {
     const classList =
       "breadcrumb bg-blue-500 block md:inline mb-1 text-white font-bold px-4 border-blue-600 rounded mr-2"
     let breadcrumbs = [{
-      class: classList + (this.part === -1 ? "border-b-4" : ""),
+      class: classList + (this.part === -1 ? " border-b-4" : ""),
       href: `${this.base_url}/introduction`,
       name: "Introduction"
     }];
     for (let i = 1; i <= this.parts; i += 1) {
       breadcrumbs.push({
-        class: classList + (this.part === i ? "border-b-4" : ""),
+        class: classList + (this.part === i ? " border-b-4" : ""),
         href: `${this.base_url}/part-${i}`,
         name: `Part ${i}`,
       });
