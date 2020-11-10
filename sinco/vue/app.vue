4a6a2c65d602783b84c212701949e473df25b256
Edward Bebbington <edward.bebbington@intercity.technology>
2020-11-10 01:16:47 +0000

    [cinco-docs] Add empty latest news to sidebar

--- sinco/vue/app.vue
+++ sinco/vue/app.vue
@@ -20,6 +20,7 @@ export default {
             Importing: "/#importing",
             Features: "/#features"
           },
+          "Latest News": {},
           Tutorials: {
             "Click Elements": "/tutorials/clicking-elements",
             "Get And Set Input Value": "/tutorials/get-and-set-input-value",
