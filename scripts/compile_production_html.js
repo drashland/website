92ccd39622b9c74eb7f7191f3b53f1ce39cd2d3d
Eric Crooks <eric.crooks@gmail.com>
2020-11-17 05:10:03 -0500

    [#versioned-docs-builder] DRY code

--- scripts/compile_production_html.js
+++ scripts/compile_production_html.js
@@ -3,31 +3,23 @@ const configs = require("../configs.json");
 const buildVersion = new Date().getTime().toString();
 let html;
 
-// Create the /index.html
-html = fs.readFileSync("./index.template.html", "utf8");
-html = html.replace(/\{\{ base_url \}\}/g, configs.base_urls.staging);
-fs.writeFileSync("./index.html", html);
+compile("landing");
+compile("dmm");
+compile("drash");
+compile("rhum");
+// build("sinco");
+compile("wocket");
 
-// Create dmm's index.html file
-html = fs.readFileSync("./dmm/index.template.html", "utf8");
-html = html.replace(/\{\{ environment \}\}/g, "production");
-html = html.replace(/\{\{ version \}\}/g, buildVersion);
-fs.writeFileSync("./dmm/index.html", html);
+function compile(module) {
+  if (module == "landing") {
+    html = fs.readFileSync("./index.template.html", "utf8");
+    html = html.replace(/\{\{ base_url \}\}/g, configs.base_urls.staging);
+    fs.writeFileSync("./index.html", html);
+    return;
+  }
 
-// Create Drash's index.html file
-html = fs.readFileSync("./drash/index.template.html", "utf8");
-html = html.replace(/\{\{ environment \}\}/g, "production");
-html = html.replace(/\{\{ version \}\}/g, buildVersion);
-fs.writeFileSync("./drash/index.html", html);
-
-// Create Rhum's index.html file
-html = fs.readFileSync("./rhum/index.template.html", "utf8");
-html = html.replace(/\{\{ environment \}\}/g, "production");
-html = html.replace(/\{\{ version \}\}/g, buildVersion);
-fs.writeFileSync("./rhum/index.html", html);
-
-// Create Wockets index.html file
-html = fs.readFileSync("./wocket/index.template.html", "utf8");
-html = html.replace(/\{\{ environment \}\}/g, "production");
-html = html.replace(/\{\{ version \}\}/g, buildVersion);
-fs.writeFileSync("./wocket/index.html", html);
+  html = fs.readFileSync("./index.template.dev.html", "utf8");
+  html = html.replace(/\{\{ environment \}\}/g, "production");
+  html = html.replace(/\{\{ version \}\}/g, buildVersion);
+  fs.writeFileSync(`./${module}/index.html`, html);
+}
--- scripts/compile_staging_html.js
+++ scripts/compile_staging_html.js
@@ -3,31 +3,23 @@ const configs = require("../configs.json");
 const buildVersion = new Date().getTime().toString();
 let html;
 
-// Create the /staging/index.html
-html = fs.readFileSync("./index.template.html", "utf8");
-html = html.replace(/\{\{ base_url \}\}/g, configs.base_urls.staging);
-fs.writeFileSync("./staging/index.html", html);
+compile("landing");
+compile("dmm");
+compile("drash");
+compile("rhum");
+// build("sinco");
+compile("wocket");
 
-// Create dmm's staging/index.html file
-html = fs.readFileSync("./index.template.dev.html", "utf8");
-html = html.replace(/\{\{ environment \}\}/g, "staging");
-html = html.replace(/\{\{ version \}\}/g, buildVersion);
-fs.writeFileSync("./staging/dmm/index.html", html);
+function compile(module) {
+  if (module == "landing") {
+    html = fs.readFileSync("./index.template.html", "utf8");
+    html = html.replace(/\{\{ base_url \}\}/g, configs.base_urls.staging);
+    fs.writeFileSync("./staging/index.html", html);
+    return;
+  }
 
-// Create Drash's staging/index.html file
-html = fs.readFileSync("./index.template.dev.html", "utf8");
-html = html.replace(/\{\{ environment \}\}/g, "staging");
-html = html.replace(/\{\{ version \}\}/g, buildVersion);
-fs.writeFileSync("./staging/drash/index.html", html);
-
-// Create Rhum's staging/index.html file
-html = fs.readFileSync("./index.template.dev.html", "utf8");
-html = html.replace(/\{\{ environment \}\}/g, "staging");
-html = html.replace(/\{\{ version \}\}/g, buildVersion);
-fs.writeFileSync("./staging/rhum/index.html", html);
-
-// Create Wockets staging/index.html file
-html = fs.readFileSync("./index.template.dev.html", "utf8");
-html = html.replace(/\{\{ environment \}\}/g, "staging");
-html = html.replace(/\{\{ version \}\}/g, buildVersion);
-fs.writeFileSync("./staging/wocket/index.html", html);
+  html = fs.readFileSync("./index.template.dev.html", "utf8");
+  html = html.replace(/\{\{ environment \}\}/g, "production");
+  html = html.replace(/\{\{ version \}\}/g, buildVersion);
+  fs.writeFileSync(`./staging/${module}/index.html`, html);
+}
