f6228d546674462c096d8e6e8fe12aa16d725662
Edward Bebbington <edward.bebbington@intercity.technology>
2020-11-18 17:22:33 +0000

    reduce serve.ts logic

--- serve.ts
+++ serve.ts
@@ -1,29 +1,15 @@
-import { readLines } from "./deps.ts";
-
 const a = await Deno.run({
   cmd: ["console/compile_vue_routes"],
-  stdout: "piped",
   stderr: "piped",
 });
 
-for await (const line of readLines(a.stdout)) {
-  try {
-    console.log(line);
-  } catch (error) {
-    console.log(line);
-  }
-}
+await a.status()
+a.close()
 
 const b = await Deno.run({
   cmd: ["deno", "run", "-A", "app.ts"],
-  stdout: "piped",
   stderr: "piped",
 });
 
-for await (const line of readLines(b.stdout)) {
-  try {
-    console.log(line);
-  } catch (error) {
-    console.log(line);
-  }
-}
+await b.status()
+b.close()
