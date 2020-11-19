4ef9c89f8e71da0d46a824f50023547b8d68c2e0
Eric Crooks <eric.crooks@gmail.com>
2020-11-19 15:52:37 -0500

    [#drash-io-api-consolidation] add staging module resource

---
+++ src/resources/staging_module_resource.ts
@@ -0,0 +1,33 @@
+import { BaseResource } from "./base_resource.ts";
+
+export class StagingModuleResource extends BaseResource {
+
+  static paths = [
+    "/staging/:module/:version?",
+  ]
+
+  //////////////////////////////////////////////////////////////////////////////
+  // FILE MARKER - METHODS - HTTP //////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+
+  public GET() {
+    const moduleName = this.request.getPathParam("module") as string;
+    const version = this.request.getPathParam("version") as string;
+
+    this.log(`Requested staging docs for "${moduleName}" module.`);
+
+    if (this.recognized_modules.indexOf(moduleName) == -1) {
+      this.log(`Module "${moduleName}" unknown.`);
+      return this.sendError(404);
+    }
+
+    if (version) {
+      this.log(`Version "${version}" was passed in.`);
+      this.log(`Sending versioned docs for "${moduleName}" module.`);
+      return this.sendVersionedDocsPage(moduleName, version);
+    }
+
+    return this.sendDocsPage(moduleName);
+  }
+}
+
