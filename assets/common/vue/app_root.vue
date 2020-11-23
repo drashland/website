7bb148e75231589f644ed9e0e11d0d656a2b13c7
Eric Crooks <eric.crooks@gmail.com>
2020-11-22 20:49:34 -0500

    [#drash-io-api-version-dropdown] fix indentation

--- assets/common/vue/app_root.vue
+++ assets/common/vue/app_root.vue
@@ -249,7 +249,7 @@ div(
         i.fa.fa-times(
           :class="{'hide': !open_sidebar}"
         )
-    environment-badge.environment-badge(:build_date="build_date")
+  environment-badge.environment-badge(:build_date="build_date")
   sidebar(
     :class="{'hide': is_mobile && !open_sidebar, 'is-mobile': is_mobile}"
     :base_url="sidebar.base_url"
