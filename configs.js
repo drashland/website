ecb9814838aefa5ae65c4549b94bfb82a2c71125
Eric Crooks <eric.crooks@gmail.com>
2020-11-26 08:47:09 -0500

    making fixes based on manual testing

--- configs.js
+++ configs.js
@@ -8,40 +8,37 @@ export const configs = {
     "latest_version": "0.78.0"
   },
   "dmm": {
-    "base_url": "/dmm/v1.3.0",
+    "base_url": "/dmm/v1.x",
     "latest_version": "v1.3.0",
     "latest_url_deno_land": "https://deno.land/x/dmm@v1.2.0/mod.ts",
     "latest_url_nest_land": "https://x.nest.land/dmm@v1.2.0/mod.ts",
     "versions": [
-      "v1.3.0",
-      "v1.2.0"
+      "v1.x"
     ]
   },
   "drash": {
-    "base_url": "/drash/v1.3.0",
+    "base_url": "/drash/v1.x",
     "latest_version": "v1.3.0",
     "latest_url_deno_land": "https://deno.land/x/drash@v1.3.0/mod.ts",
     "latest_url_nest_land": "https://x.nest.land/deno-drash@v1.3.0/mod.ts",
     "versions": [
-      "v1.3.0"
+      "v1.x"
     ]
   },
   "rhum": {
-    "base_url": "/rhum/v1.1.4",
+    "base_url": "/rhum/v1.x",
     "latest_version": "v1.1.4",
     "latest_url_deno_land": "https://deno.land/x/rhum@v1.1.4/mod.ts",
     "latest_url_nest_land": "https://x.nest.land/rhum@v1.1.4/mod.ts",
     "versions": [
-      "v1.1.4"
+      "v1.x"
     ]
   },
   "wocket": {
-    "base_url": "/wocket/v0.5.0",
+    "base_url": "/wocket/v0.x",
     "latest_version": "v0.5.0",
     "versions": [
-      "v0.6.1",
-      "v0.6.0",
-      "v0.5.0"
+      "v0.x"
     ]
   }
 };
