d38613996765da27403915ab5cae83086e8fb5e7
Eric Crooks <eric.crooks@gmail.com>
2020-11-26 05:08:10 -0500

    fix: base urls should not have trailing slashes

--- configs.js
+++ configs.js
@@ -8,7 +8,7 @@ export const configs = {
     "latest_version": "0.78.0"
   },
   "dmm": {
-    "base_url": "/dmm/v1.3.0/",
+    "base_url": "/dmm/v1.3.0",
     "latest_version": "v1.3.0",
     "latest_url_deno_land": "https://deno.land/x/dmm@v1.2.0/mod.ts",
     "latest_url_nest_land": "https://x.nest.land/dmm@v1.2.0/mod.ts",
@@ -18,7 +18,7 @@ export const configs = {
     ]
   },
   "drash": {
-    "base_url": "/drash/v1.3.0/",
+    "base_url": "/drash/v1.3.0",
     "latest_version": "v1.3.0",
     "latest_url_deno_land": "https://deno.land/x/drash@v1.3.0/mod.ts",
     "latest_url_nest_land": "https://x.nest.land/deno-drash@v1.3.0/mod.ts",
@@ -27,7 +27,7 @@ export const configs = {
     ]
   },
   "rhum": {
-    "base_url": "/rhum/v1.1.4/",
+    "base_url": "/rhum/v1.1.4",
     "latest_version": "v1.1.4",
     "latest_url_deno_land": "https://deno.land/x/rhum@v1.1.4/mod.ts",
     "latest_url_nest_land": "https://x.nest.land/rhum@v1.1.4/mod.ts",
@@ -36,7 +36,7 @@ export const configs = {
     ]
   },
   "wocket": {
-    "base_url": "/wocket/v0.5.0/",
+    "base_url": "/wocket/v0.5.0",
     "latest_version": "v0.5.0",
     "versions": [
       "v0.6.1",
