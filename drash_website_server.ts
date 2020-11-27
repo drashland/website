dafd3a07189bea0d0a8c420654e3c22ba876f12f
Eric Crooks <eric.crooks@gmail.com>
2020-11-27 07:49:24 -0500

    target assets correctly

--- drash_website_server.ts
+++ drash_website_server.ts
@@ -15,7 +15,7 @@ const server = new Drash.Http.Server({
   ],
   response_output: "text/html",
   static_paths: {
-      [`${module}-assets`]: `/assets/${module}`
+      [`${module}-assets`]: `/assets`
   },
   directory: "."
 })
