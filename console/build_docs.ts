ada416f36bcedfea300326d07759214475ddd13d
Eric Crooks <eric.crooks@gmail.com>
2020-11-28 22:11:33 -0500

    fixing scripts

--- console/build_docs.ts
+++ console/build_docs.ts
@@ -1,14 +1,11 @@
-import { run } from "./run.ts";
-import { buildWebpackBundles } from "./build_ecosystem.ts";
+import { buildDocs, run } from "./scripts.ts";
 
 const args = Deno.args.slice();
 const moduleName = args[0];
 const moduleVersion = args[1];
 
-console.log(`Building webpack bundles for ${moduleName} ${moduleVersion}`);
+console.log(`Building docs for ${moduleName} ${moduleVersion}`);
 
 await run(["npm", "install"]);
 
-await buildWebpackBundles(moduleName, moduleVersion);
-
-await run(["git", "checkout", "main"]);
+await buildDocs(moduleName, moduleVersion);
