7bd54ad2319c07a5c5a903872b885af7740a47de
Eric Crooks <eric.crooks@gmail.com>
2020-11-29 08:01:29 -0500

    make sure to checkout main and pull it

--- console/git_merge_main.ts
+++ console/git_merge_main.ts
@@ -2,6 +2,9 @@ import { gitMergeMainInto, run } from "./scripts.ts";
 
 console.log("Merging main into all branches");
 
+await run(["git", "checkout", "main"]);
+await run(["git", "pull"]);
+
 await gitMergeMainInto("dmm-v1.x");
 await gitMergeMainInto("drash-v1.x");
 await gitMergeMainInto("rhum-v1.x");
