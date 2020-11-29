b96190df5e40f5ac01f7070ab5b998343a921fb3
Eric Crooks <eric.crooks@gmail.com>
2020-11-28 22:12:25 -0500

    fix wocket version

--- console/build_ecosystem.ts
+++ console/build_ecosystem.ts
@@ -7,4 +7,4 @@ await run(["npm", "install"]);
 await buildDocs("dmm", "v1.x");
 await buildDocs("drash", "v1.x");
 await buildDocs("rhum", "v1.x");
-await buildDocs("wocket", "v1.x");
+await buildDocs("wocket", "v0.x");
