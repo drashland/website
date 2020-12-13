eed85ecab5cef08f6ddb7f2d2fd4f527972ff13c
Edward Bebbington <edward.bebbington@intercity.technology>
2020-12-13 00:24:11 +0000

    [cinco-docs] Add sinco to scripts and config

--- console/git_merge_main.ts
+++ console/git_merge_main.ts
@@ -9,5 +9,6 @@ await gitMergeMainInto("dmm-v1.x");
 await gitMergeMainInto("drash-v1.x");
 await gitMergeMainInto("rhum-v1.x");
 await gitMergeMainInto("wocket-v0.x");
+await gitMergeMainInto("sinco-v1.x");
 
 await run(["git", "checkout", "main"]);
