1c980fa84b5e44d1940e0bef11b0dab63d9c0cd5
Eric Crooks <eric.crooks@gmail.com>
2020-12-15 08:01:47 -0500

    fixing npm run dev:server script

--- console/serve_dev.ts
+++ console/serve_dev.ts
@@ -1,9 +1,9 @@
 import { run } from "./scripts.ts";
 
 const args = Deno.args.slice();
-const parentBranch = args[1]; // e.g., rhum-v1.x
+const branch = args[0]; // e.g., rhum-v1.x
 
-await run(["node", "console/compile_vue_routes.js", parentBranch]);
+await run(["node", "console/compile_vue_routes.js", branch]);
 
 await run(["pkill", "-f", "drash_website_server.ts"]);
 
