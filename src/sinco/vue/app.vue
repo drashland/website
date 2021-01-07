71bf30a877609345ee167d4ed7d25515d570806b
Edward Bebbington <edward.bebbington@intercity.technology>
2021-01-07 09:37:40 +0000

    fix hrefs being incorrect for sidebar

--- src/sinco/vue/app.vue
+++ src/sinco/vue/app.vue
@@ -9,7 +9,7 @@ export default {
     return {
       sidebar: {
         api_reference_href: "https://doc.deno.land/https/deno.land/x/sinco@" + this.$conf.sinco.latest_version + "/mod.ts",
-        base_url: this.$conf.sinco.base_url + "/#",
+        base_url: this.$conf.sinco.base_url,
         github_href: "https://github.com/drashland/sinco",
         logo: "/assets/common/img/logo_sinco.svg",
         menus: {
